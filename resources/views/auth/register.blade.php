38e1c335182dd2daf1c5f5b76a28ba5cc6ef9a8b
Mark Railton <mark@markrailton.com>
2021-04-08 23:39:59 +0100

    Make sure can not register with duplicate email

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,13 @@
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
+            <!-- Phone Number -->
+            <div class="mt-4">
+                <x-label for="phone" :value="__('Phone Number')" />
+
+                <x-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required autofocus />
+            </div>
+
             <!-- Password -->
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -30,5 +31,23 @@ class RegistrationTest extends TestCase
 
         $this->assertGuest();
         $response->assertRedirect(route('account.inactive'));
+    }
+
+    /** @test */
+    public function userCanNotRegisterWithEmailThatAlreadyExists()
+    {
+        User::factory()->create(['email' => 'test@example.com']);
+
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'phone' => '0831234567',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $this->assertGuest();
+        $this->assertDatabaseCount('users', 1);
+        $response->assertSessionHasErrors(['email' => 'The email has already been taken.']);
     }
 }
