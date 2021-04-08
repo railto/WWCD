919b69981c2db1315fd0b3885e605e546605c0ab
Mark Railton <mark@markrailton.com>
2021-04-08 19:00:54 +0100

    updating tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,5 @@ module.exports = {
         './resources/views/**/*.blade.php',
     ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-        },
-    },
-
     plugins: [require('@tailwindcss/forms')],
 };
